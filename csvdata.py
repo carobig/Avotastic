a0d02d7ae2f77e0e7aa0d91395aea63c680761a8
carobig <71708603+carobig@users.noreply.github.com>
2021-11-28 11:44:42 +0100

    Add files via upload
    created list with python

---
+++ csvdata.py
@@ -0,0 +1,9 @@
+import csv
+
+filename = 'data/Avocado_v4.csv'
+
+with open(filename, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    x = set()
+    for row in datareader:
+        print(row)
