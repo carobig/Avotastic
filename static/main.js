1194bd65d96e9e37512e718f54bf1e5fa6666564
carobig <71708603+carobig@users.noreply.github.com>
2021-12-12 20:03:34 +0100

    bilder src angepasst

--- static/main.js
+++ static/main.js
@@ -100,14 +100,14 @@ Highcharts.mapChart('region', {
             maxColor: '#006400',
             stops: [
                 [0, '#edfded'],
-                [0.67, '#7ef17e'],
-                [1, '#006400']
+                [250, '#7ef17e'],
+                [500, '#006400']
             ]
         },
         series: [{
             data: [{
                 "value": 438,
-                "code": "NJ"
+                "code": "FL"
             },
             {
                 "value": 387.35,
@@ -128,76 +128,11 @@ Highcharts.mapChart('region', {
         }]
  })
 
-/*Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/us-population-density.json', function (data) {
 
-    // Make codes uppercase to match the map data
-    data.forEach(function (p) {
-        p.code = p.code.toUpperCase();
-    });
-
-    // Instantiate the map
-    Highcharts.mapChart('region', {
-
-        chart: {
-            map: 'countries/us/us-all',
-            borderWidth: 1
-        },
-
-        title: {
-            text: 'Menge der verkauften Avocados (in Tausend)'
-        },
-
-        exporting: {
-            sourceWidth: 600,
-            sourceHeight: 500
-        },
-
-        legend: {
-            layout: 'horizontal',
-            borderWidth: 0,
-            backgroundColor: 'rgba(255,255,255,0.8)',
-            floating: true,
-            verticalAlign: 'top',
-            y: 25
-        },
-
-        mapNavigation: {
-            enabled: true
-        },
-
-        colorAxis: {
-            min: 1,
-            type: 'logarithmic',
-            minColor: '#edfded',
-            maxColor: '#006400',
-            stops: [
-                [0, '#edfded'],
-                [0.67, '#7ef17e'],
-                [1, '#006400']
-            ]
-        },
-
-        series: [{
-            animation: {
-                duration: 1000
-            },
-            data: data,
-            joinBy: ['postal-code', 'code'],
-            dataLabels: {
-                enabled: true,
-                color: '#FFFFFF',
-                format: '{point.code}'
-            },
-            name: 'Menge der verkauften Avocados',
-            tooltip: {
-                pointFormat: '{point.code}: {point.value}/km²'
-            }
-        }]
-    });
-});*/
 
 // Static 3: MENGE DER VERKAUFTEN AVOCADOS IM VERLAUF DER ZEIT
 
+//Berechnungen
 $.get('data', function(result) {
     const resultSliced = result.slice();
     let conventional15 = 0
@@ -238,7 +173,7 @@ $.get('data', function(result) {
     console.log('Conventional 2018: ' + conventional18)
     console.log('Organic 2018: ' + organic18)
 
-
+//Chart-Template
 Highcharts.chart('menge', {
     chart: {
         type: 'column'
