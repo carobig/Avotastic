66e0387c007c20d92ac58658c5305432bdef87f4
aendruu <71711533+aendruu@users.noreply.github.com>
2022-01-06 17:26:34 +0100

    Kommentare

--- static/csvdata.py
+++ static/csvdata.py
@@ -1,10 +1,13 @@
+# Hier werden die Daten der Avocados importiert.
 import csv
 
 filename = '../data/Avocado_v5.csv'
 
+# Daten werden als read-only geöffnet.
 with open(filename, 'r') as csvfile:
+    # Datensatz wird von einem CSV-File in ein Dictionary-Reader umgewandelt und mit der Funktion "delimiter" werden die Daten nach ";" unterteilt.
     datareader = csv.DictReader(csvfile, delimiter=';')
-    #Datensatz mit "delimiter" aufteilen nach ";"
+    # Alle Daten werden in eine grosse Liste geladen.
     data = list(datareader)
+    # Daten werden ausgegeben.
     print(data)
-    #alle Daten in eine grosse Liste laden
